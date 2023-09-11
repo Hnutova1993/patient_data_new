5dcf85520804aa78f2264a58ee223974c28eef7f
Embedded1993 <valeriiahnutova1993@outlook.com>
2023-09-11 15:30:00 +0900

    implemented sql

--- main.py
+++ main.py
@@ -11,9 +11,33 @@ import time
 import os
 import json
 from datetime import datetime
+import mysql.connector
 
 base_link = "https://salute.regione.veneto.it/servizi/cerca-medici-e-pediatri"
 GP_link = "https://salute.regione.veneto.it/servizi/cerca-medici-e-pediatri?p_p_id=MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-3&p_p_col_count=2&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_action=ricerca&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_d-445503-p={page}&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_form-submit=true"
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="gp"
+)
+
+
+
+cursor = db.cursor()
+cursor.execute("create database IF NOT EXISTS gp")
+
+Q1 = "CREATE TABLE IF NOT EXISTS GPs (id int PRIMARY KEY , name VARCHAR(50), address VARCHAR(100), phone_number VARCHAR(50))"
+Q2 = "CREATE TABLE IF NOT EXISTS Second_Clinic (userId int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES GPs(id), address VARCHAR(100), phone_number VARCHAR(50))"
+
+cursor.execute(Q1)
+cursor.execute(Q2)
+
+cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+
+Q3 = "INSERT INTO GPs (id, name, address, phone_number) VALUES (%s, %s, %s, %s)"
+Q4 = "INSERT INTO Second_Clinic (userId, address, phone_number) VALUES (%s, %s, %s)"
 
 def test():
     opts = uc.ChromeOptions()
@@ -72,13 +96,25 @@ def test():
                     formated[data_list[0]] = {"1st_pane":{"1": data_list[1],"time_table":json.loads(result1)},"2nd_pane":result2}
                 else:                                            
                     formated[data_list[0]] = {"1st_pane":{"1": data_list[1],"time_table":json.loads(result1)},"2nd_pane":result2,"3rd_pane":json.loads(result3)}
-                print(page,data_list[0])
+                print(page,data_list[0],"--", re.sub("\D", "", ele))
+
                 r= json.dumps(formated)
                 # print(json.loads(r))
-                with open(province+'.txt', 'w',encoding='utf-8') as json_file:
-                    # json_file.write(json.loads(r))
+                cursor.execute(Q3, (re.sub("\D", "", ele),data_list[0], result2['Ambulatorio principale']['address'],result2['Ambulatorio principale']['phone']))
+                try:
+                    print(result2['Ambulatorio secondario'])
+                    cursor.execute(Q4, (re.sub("\D", "", ele), result2['Ambulatorio secondario']['address'],result2['Ambulatorio secondario']['phone']))
+                except KeyError:
+                    # cursor.execute(Q4, (re.sub("\D", "", ele), "NULL","NULL"))
+                    pass
+                    
+
+                db.commit()
+                with open('test.txt', 'w',encoding='utf-8') as json_file:
+                    # json_file.write(json.sloads(r))
                     json.dump(json.loads(r), json_file, ensure_ascii=False)
                 driver.back()
+
 
             page += 1
             if len(elements) != 5:

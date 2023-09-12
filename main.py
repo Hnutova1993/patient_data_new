48fe5d0f6c16f670fb6a128a2b255563fc4c98a0
Embedded1993 <valeriiahnutova1993@outlook.com>
2023-09-12 15:39:43 +0900

    update entity

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 import json
 from datetime import datetime
 import mysql.connector
+from difflib import SequenceMatcher
 
 base_link = "https://salute.regione.veneto.it/servizi/cerca-medici-e-pediatri"
 GP_link = "https://salute.regione.veneto.it/servizi/cerca-medici-e-pediatri?p_p_id=MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-3&p_p_col_count=2&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_action=ricerca&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_d-445503-p={page}&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_form-submit=true"
@@ -28,23 +29,26 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 cursor.execute("create database IF NOT EXISTS gp")
 
-Q1 = "CREATE TABLE IF NOT EXISTS GPs (id int PRIMARY KEY , name VARCHAR(50), address VARCHAR(100), phone_number VARCHAR(50))"
+Q1 = "CREATE TABLE IF NOT EXISTS GPs (id int PRIMARY KEY , name VARCHAR(50), group_type VARCHAR(50))"
 Q2 = "CREATE TABLE IF NOT EXISTS Second_Clinic (userId int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES GPs(id), address VARCHAR(100), phone_number VARCHAR(50))"
+Q3 = "CREATE TABLE IF NOT EXISTS GPs_Group (userId int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES GPs(id), address VARCHAR(100), phone_number VARCHAR(50))"
 
 cursor.execute(Q1)
 cursor.execute(Q2)
+cursor.execute(Q3)
 
 cursor.execute("SET FOREIGN_KEY_CHECKS=0")
 
-Q3 = "INSERT INTO GPs (id, name, address, phone_number) VALUES (%s, %s, %s, %s)"
-Q4 = "INSERT INTO Second_Clinic (userId, address, phone_number) VALUES (%s, %s, %s)"
+Q4 = "INSERT INTO GPs (id, name, group_type) VALUES (%s, %s, %s)"
+Q5 = "INSERT INTO Second_Clinic (userId, address, phone_number) VALUES (%s, %s, %s)"
+Q6 = "INSERT INTO GPs_Group (userId, address, phone_number) VALUES (%s, %s, %s)"
 
 def test():
     opts = uc.ChromeOptions()
     # driver = uc.Chrome(driver_executable_path= ChromeDriverManager(version='116.0.5845.141').install(), options=opts)
     driver = uc.Chrome(driver_executable_path="chromedriver", options=opts)
     wait = WebDriverWait(driver,30)
-    provinces = ['Belluno','Padova','Rovigo','Treviso','Venezia','Vicenza','Verona']
+    provinces = ['Belluno']   #['Belluno','Padova','Rovigo','Treviso','Venezia','Vicenza','Verona']
     week = ['Lunedì','Martedì','Mercoledì','Giovedì','Venerdì','Sabato','Domenica']
     ambulatorio = ['Ambulatorio principale','Ambulatorio secondario','Ambulatorio terzo','Ambulatorio il quarto','Ambulatorio quinto','Ambulatorio sesto']
     page = 1
@@ -66,7 +70,9 @@ def test():
                 data_list = data.split('\n')
                 while("" in data_list):
                     data_list.remove("")
+
                 table = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "results")))
+
                 result1 = html_to_json(table[0].get_attribute('innerHTML')).replace("\\u00e0","")
                 last_name = [s for s in data_list if "COGNOME" in s]
                 if len(last_name) > 0 :
@@ -94,20 +100,30 @@ def test():
                 # print(data_list)
                 if result3 == "":
                     formated[data_list[0]] = {"1st_pane":{"1": data_list[1],"time_table":json.loads(result1)},"2nd_pane":result2}
+                    group_id = 0
                 else:                                            
                     formated[data_list[0]] = {"1st_pane":{"1": data_list[1],"time_table":json.loads(result1)},"2nd_pane":result2,"3rd_pane":json.loads(result3)}
-                print(page,data_list[0],"--", re.sub("\D", "", ele))
+                    s = 0
+                    for d in json.loads(result3):
+                        s += similar(result2['Ambulatorio principale']['address'], d['ambulatorio principale'])
+                    rate = s / len(json.loads(result3))
+                    if rate < 0.8:
+                        group_id = 1
+                    else:
+                        group_id = 2
+
+                print(page,data_list[0],"regoin code : ", re.sub("\D", "", ele), "group id : ", group_id)
 
                 r= json.dumps(formated)
                 # print(json.loads(r))
-                cursor.execute(Q3, (re.sub("\D", "", ele),data_list[0], result2['Ambulatorio principale']['address'],result2['Ambulatorio principale']['phone']))
+                cursor.execute(Q4, (re.sub("\D", "", ele),data_list[0],group_id))
                 try:
                     print(result2['Ambulatorio secondario'])
-                    cursor.execute(Q4, (re.sub("\D", "", ele), result2['Ambulatorio secondario']['address'],result2['Ambulatorio secondario']['phone']))
+                    cursor.execute(Q5, (re.sub("\D", "", ele), result2['Ambulatorio secondario']['address'],result2['Ambulatorio secondario']['phone']))
                 except KeyError:
                     # cursor.execute(Q4, (re.sub("\D", "", ele), "NULL","NULL"))
                     pass
-                    
+                cursor.execute(Q6, (re.sub("\D", "", ele), result2['Ambulatorio principale']['address'],result2['Ambulatorio principale']['phone']))
 
                 db.commit()
                 with open('test.txt', 'w',encoding='utf-8') as json_file:
@@ -157,6 +173,9 @@ def html_to_json(content, indent=None):
         if items:
             data.append(items)
     return json.dumps(data, indent=indent)
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 if __name__ == "__main__":
 

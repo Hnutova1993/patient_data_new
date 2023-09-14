94bc4c58dfe0b2a6b126bc31c7374eb42cf00952
andre_98 <andrea.scarpa60@gmail.com>
2023-09-14 07:32:59 +0200

    remote gp

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ base_link = "https://salute.regione.veneto.it/servizi/cerca-medici-e-pediatri"
 GP_link = "https://salute.regione.veneto.it/servizi/cerca-medici-e-pediatri?p_p_id=MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-3&p_p_col_count=2&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_action=ricerca&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_d-445503-p={page}&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_form-submit=true"
 
 db = mysql.connector.connect(
-  host="localhost",
+  host="app.lagoonmed.eu",
   user="root",
-  password="",
+  password="P@xtibi2021!",
   database="gp"
 )
 
@@ -29,10 +29,9 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 cursor.execute("create database IF NOT EXISTS gp")
 
-Q1 = "CREATE TABLE IF NOT EXISTS GPs (id int PRIMARY KEY , name VARCHAR(50), group_type VARCHAR(50))"
+Q1 = "CREATE TABLE IF NOT EXISTS GPs (id int PRIMARY KEY , name VARCHAR(50), group_type VARCHAR(50),province VARCHAR(20))"
 Q2 = "CREATE TABLE IF NOT EXISTS Second_Clinic (userId int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES GPs(id), address VARCHAR(100), phone_number VARCHAR(50))"
 Q3 = "CREATE TABLE IF NOT EXISTS GPs_Group (userId int PRIMARY KEY, FOREIGN KEY(userId) REFERENCES GPs(id), address VARCHAR(100), phone_number VARCHAR(50))"
-
 cursor.execute(Q1)
 cursor.execute(Q2)
 cursor.execute(Q3)
@@ -48,7 +47,7 @@ ambulatorio = ['Ambulatorio principale','Ambulatorio secondario','Ambulatorio te
 
 
 
-Q4 = "INSERT INTO GPs (id, name, group_type) VALUES (%s, %s, %s)"
+Q4 = "INSERT INTO GPs (id, name, group_type,province) VALUES (%s, %s, %s,%s)"
 Q5 = "INSERT INTO Second_Clinic (userId, address, phone_number) VALUES (%s, %s, %s)"
 Q6 = "INSERT INTO GPs_Group (userId, address, phone_number) VALUES (%s, %s, %s)"
 
@@ -134,11 +133,11 @@ def scanProvince(province):
                 else:
                     group_id = 2
 
-            print(page,data_list[0],"region code : ", re.sub("\D", "", ele), "group id : ", group_id)
+            print(page,data_list[0],"region code : ", re.sub("\D", "", ele), "group id : ", group_id,province)
 
             r= json.dumps(formated)
             # print(json.loads(r))
-            cursor.execute(Q4, (re.sub("\D", "", ele),data_list[0],group_id))
+            cursor.execute(Q4, (re.sub("\D", "", ele),data_list[0],group_id,province))
             try:
                 print(result2['Ambulatorio secondario'])
                 cursor.execute(Q5, (re.sub("\D", "", ele), result2['Ambulatorio secondario']['address'],result2['Ambulatorio secondario']['phone']))
